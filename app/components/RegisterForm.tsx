ee4eab619d0149ed4acf5091289656dec3d4a798
UbGreat <ubohodickson@gmail.com>
2025-11-04 20:20:52 +0100

    commented out api/auth codes to allow deployment

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -6,6 +6,18 @@ export default function RegisterForm() {
         <div className='shadow-lg p-5 rounded-lg border-t-4 border-green-400'>
             <h1 className='text-xl font-bold my-4 text-center'>Register</h1>
             <form className='flex flex-col gap-3'>
+                <label htmlFor="role"> Select Role</label>
+                <select name="role" id="" >
+                    <option value="PATIENT">PATIENT</option>
+                    <option value="DOCTOR">DOCTOR</option>
+                    <option value="NURSE">NURSE</option>
+                    <option value="PHARMACIST">PHARMACIST</option>
+                    <option value="LAB_SCIENTIST">LAB SCIENTIST</option>
+                    <option value="SUPPLIER">SUIPPLIER</option>
+                </select>
+                <input type="text" placeholder='First Name' />
+                <input type="text" placeholder='Surname' />
+                <input type="text" placeholder='Other names' />
                 <input type="text" placeholder='Email' />
                 <input type="password" placeholder='Password' />
                 <button className='bg-green-600 text-white font-bold cursor-pointer px-6 py-2'>Register</button>
